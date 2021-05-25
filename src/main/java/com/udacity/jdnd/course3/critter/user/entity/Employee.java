96d42d41d8324ae77974fe68a6bc585923df059d
Peter E <peter_mcdohl@yahoo.com>
2021-05-25 14:12:30 +0700

    Change employee entity

--- src/main/java/com/udacity/jdnd/course3/critter/user/entity/Employee.java
+++ src/main/java/com/udacity/jdnd/course3/critter/user/entity/Employee.java
@@ -1,31 +1,35 @@
 package com.udacity.jdnd.course3.critter.user.entity;
 
+import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
+
 import javax.persistence.*;
+import java.time.DayOfWeek;
 import java.util.Set;
 
 @Entity
 public class Employee extends User {
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "employee", cascade = CascadeType.ALL)
-    private Set<EmployeeSkills> skills;
+    @ElementCollection
+    @JoinTable(name = "employee_skills")
+    private Set<EmployeeSkill> skills;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "employee", cascade = CascadeType.ALL)
-    private Set<EmployeeAvailability> daysAvailable;
+    @ElementCollection
+    @JoinTable(name = "employee_availability")
+    private Set<DayOfWeek> daysAvailable;
 
-    public Set<EmployeeSkills> getSkills() {
+    public Set<EmployeeSkill> getSkills() {
         return skills;
     }
 
-    public void setSkills(Set<EmployeeSkills> skills) {
+    public void setSkills(Set<EmployeeSkill> skills) {
         this.skills = skills;
     }
 
-
-    public Set<EmployeeAvailability> getDaysAvailable() {
+    public Set<DayOfWeek> getDaysAvailable() {
         return daysAvailable;
     }
 
-    public void setDaysAvailable(Set<EmployeeAvailability> daysAvailable) {
+    public void setDaysAvailable(Set<DayOfWeek> daysAvailable) {
         this.daysAvailable = daysAvailable;
     }
 }
